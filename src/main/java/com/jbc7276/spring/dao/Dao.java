1edf86cf9718b93f5a0fceed5e9ac84e4f6c148a
gksth46 <gksth46@naver.com>
2016-11-21 20:20:41 +0900

    123
    123

--- src/main/java/com/jbc7276/spring/dao/Dao.java
+++ src/main/java/com/jbc7276/spring/dao/Dao.java
@@ -9,6 +9,12 @@ public class Dao {
 	
 	public void TestDao2(){
 		
+		System.out.println("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ1");
+		System.out.println("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ2");
+		System.out.println("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ3");
+		System.out.println("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ4");
+		System.out.println("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ5");
 		
 	}
+	
 }
